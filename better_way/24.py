a1243c0eecc33ccf7472bce5b06bd9239429fb8b
da-head0 <andyourturntome@gmail.com>
2023-06-02 00:15:21 +0900

    24.py

--- better_way/24.py
+++ better_way/24.py
@@ -8,6 +8,7 @@ def log(message, when=None):
         message (_type_): _description_
         when (_type_, optional): _description_. Defaults to None.
     """
+    # 디폴트 인자 값으로 None을 사용하는 것은 인자가 가변적인 경우 특히 중요
     if when is None:
         when = datetime.now()
     print(f'{when}: {message}')
@@ -15,3 +16,34 @@ def log(message, when=None):
 log('hello!')
 sleep(0.1)
 log('Hello again!')
+
+import json
+
+def decode(data, default=None):
+    try:
+        return json.loads(data)
+    except ValueError:
+        if default is None:
+            default = {}
+        return default
+    
+foo = decode('wrong data')
+foo['stuff'] = 5
+bar = decode('again wrong data')
+bar['meep'] = 1
+print('Foo: ', foo) 
+print('Bar:', bar) # foo와 bar는 동일한 딕셔너리 객체
+
+# assert foo is bar
+
+
+from typing import Optional
+
+def log_typed(message: str,
+              when: Optional[datetime]=None) -> None:
+    if when is None:
+        when = datetime.now()
+    print(f'{when}: {message}')
+
+        
+log_typed('뭐라뭐라')
